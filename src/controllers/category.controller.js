0d21bf7fac22ccd4fa29df05501fd85db8c7e573
amitj <amitjangra94664@gmail.com>
2026-02-19 16:24:09 +0530

    category-api-added

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -17,3 +17,13 @@ exports.createCategory = async (req, res) => {
          } catch (error) {
             res.status(500).json({ error: error.message });
         }   };
+       exports.getCategoryById = async (req, res) => {
+        try {
+            const category = await CategoryModel.findById(req.params.id);   
+            if (!category) {
+                return res.status(404).json({ error: "Category not found" });
+            }
+            res.status(200).json({status: "success", data: category });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }   };  
--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,12 +1,12 @@
 const express= require("express");
 const router= express.Router();
 
-const { createCategory, getAllCategories } = require("../controllers/category.controller");
+const { createCategory, getAllCategories,getCategoryById } = require("../controllers/category.controller");
 // ✅ Test route
 router.get("/test", (req, res) => {
   res.send("Category API working");
 });
 router.post("/createCategory", createCategory);
 router.get("/getAllCategories", getAllCategories);
-
+router.get("/getCategoryById/:id", getCategoryById);
 module.exports= router;
