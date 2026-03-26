3353c3ea4cce4e6d2cc46d0a08e8023dac3eb249
amitj <amitjangra94664@gmail.com>
2026-03-26 14:50:07 +0530

    fixing-error

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -11,24 +11,11 @@ const {
   deleteProduct,
 } = require("../controllers/product.controller");
 
-// 🔥 SAFE MULTER WRAPPER
-const uploadSingle = (req, res, next) => {
-  upload.single("image")(req, res, (err) => {
-    if (err) {
-      return res.status(400).json({
-        message: err.message,
-      });
-    }
-    next();
-  });
-};
+// ✅ SIMPLE & STABLE
+router.post("/createProduct", upload.single("image"), createProduct);
 
-router.post("/createProduct", uploadSingle, createProduct);
+router.put("/updateProduct/:id", upload.single("image"), updateProduct);
 
-// ✅ UPDATE
-router.put("/updateProduct/:id", uploadSingle, updateProduct);
-
-// बाकी routes
 router.get("/getAllProducts", getProducts);
 router.get("/getProductById/:id", getProductById);
 router.delete("/deleteProduct/:id", deleteProduct);
